5c0dde3f4d3d42a62b7feae54ad949d6fdc069fc
SamratAdhikari <samratmetaladhikari@gmail.com>
2024-12-18 03:08:01 +0545

    add Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { Box } from "@mui/material";
 import Login from "./pages/Login/Login";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 
 function App() {
+    const { authUser } = useAuthContext();
     return (
-        <Box className="flex justify-center items-center w-screen h-auto bg-primary">
-            <Login />
+        <Box className="flex justify-center items-center w-full h-auto bg-primary">
+            <Routes>
+                <Route
+                    path="/"
+                    element={authUser ? <Navigate to="/" /> : <Login />}
+                />
+                <Route path="/signup" />
+            </Routes>
+            <Toaster />
         </Box>
     );
 }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,72 +1,112 @@
-import { Box, FormControl, FormHelperText, TextField } from "@mui/material";
+import { Link } from "react-router-dom";
+import useLogin from "../../hooks/useLogin";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import {
+    Box,
+    Button,
+    CircularProgress,
+    FormControl,
+    FormHelperText,
+    TextField,
+} from "@mui/material";
 import { Formik } from "formik";
 import * as Yup from "yup";
+import { useState } from "react";
 
 const Login = () => {
+    const { loading, login } = useLogin();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await login(username, password);
+    };
+
     return (
-        <Box className="h-screen w-full flex flex-col justify-center items-center">
-            <Box className="flex flex-col justify-center items-center bg-white rounded-md p-8 w-[60%] sm:w-[70%] md:w-[40%] gap-4 h-[30%]">
-                {/* title */}
-                <h4 className="font-semibold text-2xl text-gray-700">
-                    Login |{" "}
-                    <span className="text-primary font-semibold text-2xl">
-                        TreasureHunt
-                    </span>
-                </h4>
+        <Box className="h-screen flex justify-center items-center">
+            <Box className="flex flex-col items-center justify-center min-w-96 mx-auto bg-white rounded-lg">
+                <Box className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+                    <h1 className="text-3xl font-semibold text-center text-gray-600 mb-8">
+                        Login
+                        <span className="text-primary"> TreasureHunt</span>
+                    </h1>
 
-                {/* Form */}
-                <Formik
-                    initialValues={{
-                        username: "",
-                        password: "",
-                    }}
-                    validationSchema={Yup.object({
-                        username: Yup.string()
-                            .required("Username is required.")
-                            .trim()
-                            .max(25, "Username must be 25 characters max"),
-                    })}
-                    onSubmit={(values) => {
-                        console.log(values);
-                    }}
-                >
-                    {(formik) => {
-                        return (
-                            <form
-                                onSubmit={formik.handleSubmit}
-                                className="flex justify-center flex-col gap-4"
-                            >
-                                <FormControl>
-                                    <TextField
-                                        label="Username"
-                                        {...formik.getFieldProps("username")}
-                                        color="info"
-                                    />
-                                    {formik.touched.username &&
-                                    formik.errors.username ? (
-                                        <FormHelperText error>
-                                            {formik.errors.username}
-                                        </FormHelperText>
-                                    ) : null}
-                                </FormControl>
+                    {/* Form */}
+                    <Formik
+                        initialValues={{
+                            username: "",
+                            password: "",
+                        }}
+                        validationSchema={Yup.object({
+                            username: Yup.string()
+                                .required("Username is required.")
+                                .trim()
+                                .max(25, "Username must be 25 characters max"),
+                            password: Yup.string()
+                                .required("Password is required.")
+                                .trim()
+                                .max(25, "Password must be 25 characters max"),
+                        })}
+                        onSubmit={(values) => {
+                            console.log(values);
+                        }}
+                    >
+                        {(formik) => {
+                            return (
+                                <form
+                                    onSubmit={formik.handleSubmit}
+                                    className="flex justify-center flex-col gap-4"
+                                >
+                                    <FormControl>
+                                        <TextField
+                                            label="Username"
+                                            {...formik.getFieldProps(
+                                                "username"
+                                            )}
+                                            color="info"
+                                        />
+                                        {formik.touched.username &&
+                                        formik.errors.username ? (
+                                            <FormHelperText error>
+                                                {formik.errors.username}
+                                            </FormHelperText>
+                                        ) : null}
+                                    </FormControl>
 
-                                <FormControl>
-                                    <TextField
-                                        label="Password"
-                                        {...formik.getFieldProps("password")}
-                                        color="info"
-                                    />
-                                    {formik.touched.password &&
-                                    formik.errors.password ? (
-                                        <FormHelperText error>
-                                            {formik.errors.password}
-                                        </FormHelperText>
-                                    ) : null}
-                                </FormControl>
-                            </form>
-                        );
-                    }}
-                </Formik>
+                                    <FormControl>
+                                        <TextField
+                                            label="Password"
+                                            type="password"
+                                            {...formik.getFieldProps(
+                                                "password"
+                                            )}
+                                            color="info"
+                                        />
+                                        {formik.touched.password &&
+                                        formik.errors.password ? (
+                                            <FormHelperText error>
+                                                {formik.errors.password}
+                                            </FormHelperText>
+                                        ) : null}
+                                    </FormControl>
+
+                                    <Button
+                                        variant="contained"
+                                        type="submit"
+                                        sx={{
+                                            bgcolor: "#6A5AE0",
+                                            textTransform: "none",
+                                            paddingY: 1,
+                                            fontSize: "1rem",
+                                        }}
+                                    >
+                                        Submit
+                                    </Button>
+                                </form>
+                            );
+                        }}
+                    </Formik>
+                </Box>
             </Box>
         </Box>
     );
